6751c720f1d13f5562207e876220e5d972c78b30
GANitak <freemindys@gmail.com>
2022-01-16 20:40:46 +0100

    src/Apps.js  & svg FACELOGO

--- ganitak/src/App.js
+++ ganitak/src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
+import IMGanit from './IMGanit.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="Header">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={IMGanit} className="Face-logo" alt="FaceLogo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
