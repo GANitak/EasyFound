dc9399f35304f75d23ffabdeaedeb0da711ae179
GANitak <freemindys@gmail.com>
2022-01-16 21:12:19 +0100

    src/ App.css fix & Apps.js

--- ganitak/src/App.js
+++ ganitak/src/App.js
@@ -4,12 +4,20 @@ import './App.css';
 function App() {
   return (
     <div className="Header">
-      <header className="App-header">
+    <nav bar="contact" style={{ color: '#369f14'}}>Contact</nav>
+          <header className="App-header">
         <img src={IMGanit} className="Face-logo" alt="FaceLogo" />
-        <p>
-          Edit <code>src/App.js</code>
-        </p>
+        <main>
+            <p><strong>Welcome to GANit_ak.io</strong></p>
+            <p>
+      :: What they do :: <br/>
+        <em>web & mobile app development<br/></em>
+        (front end) & back end<br/>
+          <strong><code>Happy times G0 after that.</code></strong>
+            </p>
+            </main>
       </header>
+      <footer className="footer">&copy; &#8471; GANitak.io</footer>
     </div>
   );
 }
